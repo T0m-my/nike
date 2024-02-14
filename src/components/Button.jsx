c2fb510aeef37021263ea62ef294acfec548ff37
Thomas Mumena <mumenathomas@gmail.com>
2024-02-14 15:59:41 +0200

    Create a Super Quality section. Add styles.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,11 @@ const Button = ({label, iconURL}) => {
   return (
     <button className="border border-x-coral-red bg-coral-red text-white rounded-full px-7 py-4 font-montserrat text-lg leading-none flex justify-center items-center gap-2">
       {label}
-      <img 
+      {iconURL && <img 
         src={iconURL} 
         alt="Arrow right icon" 
         className="w-5 h-5 ml-2 rounded-full" 
-      />
+      />}
     </button>
   )
 }
